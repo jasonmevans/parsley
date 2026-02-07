2ed3ff28322bd11eddc956f8787ae11793ff69d5
Jason Evans <505818+jasonmevans@users.noreply.github.com>
2026-02-06 22:09:12 -0500

    Add jest

--- eslint.config.js
+++ eslint.config.js
@@ -1,66 +1,57 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import json from "@eslint/json";
-import markdown from "@eslint/markdown";
-import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import css from '@eslint/css';
+import { defineConfig } from 'eslint/config';
 
-export default defineConfig(
-  [
-      {
-      files: [
-        "**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"
-      ],
-      plugins: {
-        js
-      },
-      extends: [
-        "js/recommended"
-      ],
-      languageOptions: {
-        globals: {
-          ...globals.browser,
-          ...globals.node
-        }
-      }
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: {
+      js,
     },
-    tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
-    {
-      files: ["**/*.json"],
-      plugins: {
-        json
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
       },
-      language: "json/json",
-      extends: [
-        "json/recommended"
-      ]
     },
-    {
-      files: [
-        "**/*.md"
-      ],
-      plugins: {
-        markdown
-      },
-      language: "markdown/gfm",
-      extends: [
-        "markdown/recommended"
-      ]
+  },
+  // {
+  //   files: ['**/*.test.{js,ts}'],
+  //   plugins: ['jest'],
+  //   env: {
+  //     'jest/globals': true,
+  //   },
+  // },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.json'],
+    plugins: {
+      json,
     },
-    {
-      files: [
-        "**/*.css"
-      ],
-      plugins: {
-        css
-      },
-      language: "css/css",
-      extends: [
-        "css/recommended"
-      ]
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.md'],
+    plugins: {
+      markdown,
     },
-  ]
-);
+    language: 'markdown/gfm',
+    extends: ['markdown/recommended'],
+  },
+  {
+    files: ['**/*.css'],
+    plugins: {
+      css,
+    },
+    language: 'css/css',
+    extends: ['css/recommended'],
+  },
+]);
