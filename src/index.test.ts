b929a887637ce7fa4835e986f22a6bf8ea25abe0
Jason Evans <505818+jasonmevans@users.noreply.github.com>
2026-02-10 21:19:16 -0500

    Add run function

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,11 @@
-import { str } from './';
+import { run, str } from './';
 
 describe('Basic string parsers', () => {
   it('should match a string', () => {
     const parser = str('Hello there!');
 
-    expect(() => parser('This is not correct')).toThrow();
+    expect(() => run(parser, 'This is not correct')).toThrow();
 
-    expect(parser('Hello there!')).toBe('Hello there!');
+    expect(run(parser, 'Hello there!')).toBe('Hello there!');
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-export const str = (s: string) => (targetString: string) => {
-  if (targetString.startsWith(s)) {
-    return s;
+export type Matcher<T> = (input: T) => T;
+export type Parser<T> = (target: T) => Matcher<T>;
+
+export const str: Parser<string> = (target) => (input) => {
+  if (input.startsWith(target)) {
+    return target;
   }
 
   throw new Error(
-    `Tried to match "${s}", but got "${targetString.substring(0, s.length)}"`
+    `Tried to match "${target}", but got "${input.substring(0, target.length)}"`
   );
+};
+
+export const run = <T>(parser: Matcher<T>, target: T): T => {
+  return parser(target);
 };
