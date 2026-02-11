a7858d1f61c04ddfd6277e6db2ec97ebbb5f675a
Jason Evans <505818+jasonmevans@users.noreply.github.com>
2026-02-10 21:47:14 -0500

    Introduce ParserState

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,6 +6,10 @@ describe('Basic string parsers', () => {
 
     expect(() => run(parser, 'This is not correct')).toThrow();
 
-    expect(run(parser, 'Hello there!')).toBe('Hello there!');
+    expect(run(parser, 'Hello there!')).toStrictEqual({
+      target: 'Hello there!',
+      result: 'Hello there!',
+      index: 12,
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
-export type Matcher<T> = (input: T) => T;
-export type Parser<T> = (target: T) => Matcher<T>;
+export type ParserState<T> = {
+  target: T;
+  result: T | null;
+  index: number;
+};
+export type Parser<T> = (
+  input: T
+) => (parserState: ParserState<T>) => ParserState<T>;
 
-export const str: Parser<string> = (target) => (input) => {
-  if (input.startsWith(target)) {
-    return target;
+export const str: Parser<string> = (s) => (parserState) => {
+  const { target, index } = parserState;
+
+  if (target.slice(index).startsWith(s)) {
+    return {
+      ...parserState,
+      result: s,
+      index: index + s.length,
+    };
   }
 
   throw new Error(
-    `Tried to match "${target}", but got "${input.substring(0, target.length)}"`
+    `Tried to match "${s}", but got "${target.substring(index, index + s.length)}"`
   );
 };
 
-export const run = <T>(parser: Matcher<T>, target: T): T => {
-  return parser(target);
+export const run = <T>(
+  parser: ReturnType<Parser<T>>,
+  target: T
+): ParserState<T> => {
+  const initialState = {
+    target,
+    result: null,
+    index: 0,
+  };
+
+  return parser(initialState);
 };
